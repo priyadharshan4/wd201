6ffcd92b7176ad1a0a277a636ed58403215f09b5
priyadharshan4 <priyadharshan.cse@hicet.ac.in>
2023-09-11 18:19:50 +0530

    Todo-cli update

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const todoList =  require('../todo');
 const {all, markAsComplete, add, overdue, dueToday,dueLater } = todoList();
 describe("Todolist Test Suite", () => {
